b56d9bbe795d2814f25936a012b892e249f0095c
saswat-07 <saswatupadhyayas@gmail.com>
2024-04-07 03:34:35 +0530

    angular.dev tutorial done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ RouterModule,HomeComponent],
+  // templateUrl: './app.component.html',
+  // template: `<h1>Hello World!</h1>`,
+  template: ` <main>
+    <header class="brand-name">
+    <a routerLink="/">
+    <img
+      class="brand-logo"
+      src="/assets/logo.svg"
+      alt="logo"
+      aria-hidden="true"
+    />
+    </a>
+    </header>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'official-docs-proj';
+  title = 'homes';
 }
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  // templateUrl: './housing-location.component.html',
+  template: `
+  <section class="listing">
+    <img class="listing-photo" alt="Exterior photo of {{housingLocation.name}}" [src]="housingLocation.photo" >
+    <h2 class="listing-heading">{{housingLocation.name}}</h2>
+    <p class="listing-location"> {{housingLocation.city}}, {{housingLocation.state}}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+  </section>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
